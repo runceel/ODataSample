14b4b1d0581056744f263ee48f4c20e9b52211a4
Kazuki Oota <k.ota.0130@gmail.com>
2012-02-23 00:31:48 +0900

    add domainservice

---
+++ ODataSample/EducationDomainService.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ODataSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // TODO: アプリケーション ロジックを含むメソッドを作成します。
+    [EnableClientAccess()]
+    public class EducationDomainService : DomainService
+    {
+        [Query(IsDefault = true)]
+        public IQueryable<Person> GetPeople()
+        {
+            return new[] { new Person { ID = 0, Name = "田中　太郎" } }.AsQueryable();
+        }
+    }
+}
+
+
---
+++ ODataSample/Entities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ODataSample
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
